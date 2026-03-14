b0798b8614d69a83e5157f0648dac9b296def9fe
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2026-03-14 18:45:15 +0300

    fix

--- src/Vertr.Market.Application/LocalStorage/OrderBooksLocalStorage.cs
+++ src/Vertr.Market.Application/LocalStorage/OrderBooksLocalStorage.cs
@@ -3,33 +3,64 @@ using Vertr.Common.Contracts.Abstractions;
 
 namespace Vertr.Market.Application.LocalStorage;
 
-internal sealed class OrderBooksLocalStorage : IOrderBooksLocalStorage, IMarketQuoteProvider
+internal sealed class OrderBooksLocalStorage : IOrderBooksLocalStorage
 {
-    private readonly Dictionary<Guid, OrderBookHistory> _books = [];
+    private readonly Dictionary<Guid, SortedList<DateTime, OrderBook>> _books = [];
 
-    public OrderBook? GetById(Guid instrumentId)
+    public OrderBook? GetLast(Guid instrumentId)
     {
-        _books.TryGetValue(instrumentId, out var orderBook);
-        return orderBook;
+        _books.TryGetValue(instrumentId, out var bookList);
+        return bookList?.Last().Value;
     }
 
-    public void Update(OrderBook orderBook)
+    public IEnumerable<OrderBook> GetAllBefore(Guid instrumentId, DateTime time)
     {
-        _books[orderBook.InstrumentId] = orderBook;
-    }
+        _books.TryGetValue(instrumentId, out var bookList);
 
-    public Quote? GetMarketQuote(Guid instrumentId)
-    {
-        if (!_books.TryGetValue(instrumentId, out var orderBook))
+        if (bookList == null || !bookList.Any())
         {
-            return null;
+            return [];
         }
 
-        return new Quote
+        return bookList
+            .Where(i => i.Key < time)
+            .Select(v => v.Value);
+    }
+
+    public int DeleteAllBefore(Guid instrumentId, DateTime time)
+    {
+        _books.TryGetValue(instrumentId, out var bookList);
+
+        var count = 0;
+
+        if (bookList == null || !bookList.Any())
         {
-            Time = orderBook.UpdatedAt,
-            Bid = orderBook.MaxBid,
-            Ask = orderBook.MinAsk,
-        };
+            return count;
+        }
+
+        var item = bookList.First();
+
+        while (item.Key < time && bookList.Any())
+        {
+            bookList.RemoveAt(0);
+            item = bookList.First();
+            count++;
+        }
+
+        return count;
+    }
+
+    public void Add(OrderBook orderBook)
+    {
+        var key = orderBook.InstrumentId;
+        _books.TryGetValue(key, out var list);
+
+        if (list == null)
+        {
+            list = [];
+            _books.Add(key, list);
+        }
+
+        list.Add(orderBook.UpdatedAt, orderBook);
     }
 }
