0a46eb7e60d77dc7d58899cf450a294ed54d9dea
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2026-03-15 10:27:40 +0300

    update

---
+++ src/Vertr.Market.Application/Export/Extensions/OrderBookExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Vertr.Common.Contracts;
+using Vertr.Market.Application.Export.Models;
+
+namespace Vertr.Market.Application.Export.Extensions;
+
+internal static class OrderBookExtensions
+{
+    extension(OrderBook orderBook)
+    {
+        public OrderBookDto ToDto()
+        {
+            var dto = new OrderBookDto();
+
+            var depth = orderBook.Depth;
+
+            return dto;
+        }
+    }
+}
---
+++ src/Vertr.Market.Application/Export/Models/OrderBookDto.cs
@@ -0,0 +1,46 @@
+﻿namespace Vertr.Market.Application.Export.Models;
+
+internal class OrderBookDto
+{
+    public DateTime TimeUtc { get; set; }
+    public decimal BidPrice0 { get; set; }
+    public long BidQty0 { get; set; }
+    public decimal AskPrice0 { get; set; }
+    public long AskQty0 { get; set; }
+    public decimal BidPrice1 { get; set; }
+    public long BidQty1 { get; set; }
+    public decimal AskPrice1 { get; set; }
+    public long AskQty1 { get; set; }
+    public decimal BidPrice2 { get; set; }
+    public long BidQty2 { get; set; }
+    public decimal AskPrice2 { get; set; }
+    public long AskQty2 { get; set; }
+    public decimal BidPrice3 { get; set; }
+    public long BidQty3 { get; set; }
+    public decimal AskPrice3 { get; set; }
+    public long AskQty3 { get; set; }
+    public decimal BidPrice4 { get; set; }
+    public long BidQty4 { get; set; }
+    public decimal AskPrice4 { get; set; }
+    public long AskQty4 { get; set; }
+    public decimal BidPrice5 { get; set; }
+    public long BidQty5 { get; set; }
+    public decimal AskPrice5 { get; set; }
+    public long AskQty5 { get; set; }
+    public decimal BidPrice6 { get; set; }
+    public long BidQty6 { get; set; }
+    public decimal AskPrice6 { get; set; }
+    public long AskQty6 { get; set; }
+    public decimal BidPrice7 { get; set; }
+    public long BidQty7 { get; set; }
+    public decimal AskPrice7 { get; set; }
+    public long AskQty7 { get; set; }
+    public decimal BidPrice8 { get; set; }
+    public long BidQty8 { get; set; }
+    public decimal AskPrice8 { get; set; }
+    public long AskQty8 { get; set; }
+    public decimal BidPrice9 { get; set; }
+    public long BidQty9 { get; set; }
+    public decimal AskPrice9 { get; set; }
+    public long AskQty9 { get; set; }
+}
