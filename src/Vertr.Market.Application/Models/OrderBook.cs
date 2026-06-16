289c29ea077c35b786038bd72a2c05c88c4d1e3e
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2026-06-16 22:59:50 +0300

    fix

--- src/Vertr.Market.Application/Models/OrderBook.cs
+++ src/Vertr.Market.Application/Models/OrderBook.cs
@@ -1,3 +1,29 @@
-﻿namespace Vertr.Market.Application.Models;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Vertr.Market.Application.Models;
 
 
+[StructLayout(LayoutKind.Sequential)]
+public struct OrderBook
+{
+    public int AssetId;
+    public long Timestamp;
+    public LevelBuffer Bids;
+    public LevelBuffer Asks;
+    public int BidCount;
+    public int AskCount;
+}
+
+public sealed class OrderBookEvent
+{
+    public OrderBook OrderBook;
+}
+
+public readonly record struct OrderBookLevel(decimal Price, decimal Volume);
+
+[InlineArray(10)]
+public struct LevelBuffer
+{
+    private OrderBookLevel _element0;
+}
--- src/Vertr.Market.Application/Models/OrderBookPublisher.cs
+++ src/Vertr.Market.Application/Models/OrderBookPublisher.cs
@@ -126,27 +126,3 @@ public sealed class OrderBookPublisher
         }
     }
 }
-
-[StructLayout(LayoutKind.Sequential)]
-public struct OrderBook
-{
-    public int AssetId;
-    public long Timestamp;
-    public LevelBuffer Bids;
-    public LevelBuffer Asks;
-    public int BidCount;
-    public int AskCount;
-}
-
-public sealed class OrderBookEvent
-{
-    public OrderBook OrderBook;
-}
-
-public readonly record struct OrderBookLevel(decimal Price, decimal Volume);
-
-[InlineArray(10)]
-public struct LevelBuffer
-{
-    private OrderBookLevel _element0;
-}
