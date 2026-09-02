5a4d6a4c22c544d6eb6ff30f50c65a491f00ad48
anatoly-note <anfemail@gmail.com>
2026-09-03 01:24:27 +0400

    metrics

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vertr-market
 
+## Metrics
+
+https://learn.microsoft.com/en-us/aspnet/core/metrics/overview?view=aspnetcore-10.0
+
--- src/Vertr.Market.Host/Program.cs
+++ src/Vertr.Market.Host/Program.cs
@@ -1,4 +1,6 @@
-using Serilog;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace Vertr.Market.Host;
 
@@ -9,6 +11,34 @@ public static class Program
         var builder = WebApplication.CreateBuilder(args);
         var configuration = builder.Configuration;
 
+        var otel = builder.Services.AddOpenTelemetry();
+
+        otel.ConfigureResource(resource => resource
+            .AddService(serviceName: builder.Environment.ApplicationName));
+
+        otel.WithMetrics(metrics => metrics
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation() // Метрики исходящих HTTP-запросов
+            .AddRuntimeInstrumentation()    // Метрики .NET Runtime (GC, CPU, ThreadPool)
+            .AddMeter("Microsoft.AspNetCore.Hosting")
+            .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+            .AddMeter("System.Net.Http")
+            .AddMeter("System.Net.NameResolution")
+            .AddPrometheusExporter());
+
+        /*
+        otel.WithTracing(tracing =>
+        {
+            tracing.AddAspNetCoreInstrumentation();
+            tracing.AddHttpClientInstrumentation();
+            tracing.AddConsoleExporter();
+        });
+        */
+
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
+
+        /*
         Log.Logger = new LoggerConfiguration()
             .ReadFrom.Configuration(configuration)
             .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
@@ -18,8 +48,22 @@ public static class Program
             .ReadFrom.Configuration(context.Configuration) // Read from appsettings.json
             .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
             .Enrich.WithThreadId());
+        */
 
         var app = builder.Build();
+
+        app.MapPrometheusScrapingEndpoint("met");
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/openapi/v1.json", "v1");
+            });
+        }
+
+        app.MapControllers();
 
         await app.RunAsync();
     }
